c6bb92b7c4e40b18f9e8b9916776fd36dc77eeec
Andre K <konstan@yahoo.com>
2021-04-28 00:50:46 -0400

    Initial check-in

---
+++ tunegpu_rvn.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# log beginning
+echo "Started on `date`" > /var/run/tunegpu_status
+
+# card #1 - RX580
+CHOME="/sys/class/drm/card0/device"
+
+echo "manual"       > $CHOME/power_dpm_force_performance_level
+echo "s 2 600 750" > $CHOME/pp_od_clk_voltage
+echo "s 3 610 750" > $CHOME/pp_od_clk_voltage
+echo "s 4 620 750" > $CHOME/pp_od_clk_voltage
+echo "c"            > $CHOME/pp_od_clk_voltage
+echo "0 1 2 3 4"    > $CHOME/pp_dpm_sclk
+
+# card #2 - RX550
+CHOME="/sys/class/drm/card1/device"
+echo "manual"       > $CHOME/power_dpm_force_performance_level
+echo "s 3 900 750"  > $CHOME/pp_od_clk_voltage
+echo "s 4 1000 750" > $CHOME/pp_od_clk_voltage
+echo "c"            > $CHOME/pp_od_clk_voltage
+echo "0 1 2 3 4"    > $CHOME/pp_dpm_sclk
+
+# log completion
+echo "Finished on `date`" >> /var/run/tunegpu_status
+
